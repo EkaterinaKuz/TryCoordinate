d304d6c33d7c69c3ffb66867feb020769ef09e0d
holod.malikova@yandex.ru <holod.malikova@yandex.ru>
2018-12-08 01:41:56 +0700

    Added TryCoordinate

---
+++ TryCoordinate/TryCoordinate/Point.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+
+namespace TryCoordinate
+{
+    public class Point
+    {
+
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public const double Radius = 6371; // The radius of the Earth in kilometers.
+
+        public Point(double latitude, double longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+        public static double DistanceBetweenToPoint(double startLatitude, double startLongitude, double endLatitude, double endLongitude)
+        {
+            try
+            {
+                if (!IsCorrectPoints(startLatitude, startLongitude, endLatitude, endLongitude))
+                {
+                    return 0;
+                }
+
+                var deltaLatitude = ToRadian(endLatitude - startLatitude);
+                var deltaLongitude = ToRadian(endLongitude - startLongitude);
+
+                var a = Math.Sin(deltaLatitude / 2) * Math.Sin(deltaLatitude / 2) +
+                        Math.Cos(ToRadian(startLatitude)) * Math.Cos(ToRadian(endLatitude)) *
+                        Math.Sin(deltaLongitude / 2) * Math.Sin(deltaLongitude / 2);
+
+                var angle = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+
+                return Radius * angle;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error " + ex);
+                return 0;
+            }
+        }
+
+        public double DistanceBetweenToPoints(Point endPoint)
+        {
+            if (endPoint == null)
+            {
+                return 0;
+            }
+
+            return DistanceBetweenToPoint(Latitude, Longitude, endPoint.Latitude, endPoint.Longitude);
+        }
+
+
+        private static bool IsCorrectPoints(double startLatitude, double endLatitude, double startLongitude, double endLongetude)
+        {
+            if ( (Math.Abs(startLatitude) <=90d) && (Math.Abs(startLongitude) <=180d) )
+            {
+                if ( (Math.Abs(endLatitude) <= 90d) && (Math.Abs(endLongetude) <= 180d) )
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+
+        private static double ToRadian(double deg)
+        {
+            if (deg == 0d)
+            {
+                return 0d;
+            }
+
+            return deg * Math.PI / 180;
+        }
+
+    }
+}
---
+++ TryCoordinate/UnitTests/UnitTest1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TryCoordinate;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class PointTests
+    {
+   
+        [TestMethod]
+        public void TestFirstCorrectToPoint()
+        {
+            var startPoint = new Point(0, 0);
+            var endPoint = new Point(0, 1);
+            var actual = startPoint.DistanceBetweenToPoints(endPoint);
+
+            Assert.AreEqual(expected: 111, actual: actual, delta: 0.3);
+        }
+
+        [TestMethod]
+        public void TestSecondCorrectToPoint()
+        {
+            var startPoint = new Point(89.9, 0);
+            var endPoint = new Point(89.9, 1);
+            var actual = startPoint.DistanceBetweenToPoints(endPoint);
+
+            Assert.AreEqual(expected: 0.19, actual: actual, delta: 0.3);
+        }
+
+        [TestMethod]
+        public void TestNotCorrectstartPoints()
+        {
+            var startPoint = new Point(1000.5, 3000.5);
+            var endPoint = new Point(-33.857, 9.526);
+            var actual = startPoint.DistanceBetweenToPoints(endPoint);
+
+            Assert.AreEqual(expected: 0, actual: actual, message: "Not correct startPoint point");
+        }
+
+        [TestMethod]
+        public void TestNotCorrectendPoints()
+        {
+            var startPoint = new Point(33.857, 9.526);
+            var endPoint = new Point(-1000.5, 300.5);
+            var actual = startPoint.DistanceBetweenToPoints(endPoint);
+
+            Assert.AreEqual(expected: 0, actual: actual, message: "Not correct endPoint point");
+        }
+
+        [TestMethod]
+        public void TestDistanceToPointMinValue()
+        {
+            var startPoint = new Point(double.MinValue, double.MinValue);
+            var endPoint = new Point(double.MinValue, double.MinValue);
+            var actual = startPoint.DistanceBetweenToPoints(endPoint);
+
+            Assert.AreEqual(expected: 0, actual: actual, message: "Minimum value point");
+        }
+
+        [TestMethod]
+        public void TestDistanceToPointMaxValue()
+        {
+            var startPoint = new Point(double.MaxValue, double.MaxValue);
+            var endPoint = new Point(double.MaxValue, double.MaxValue);
+            var actual = startPoint.DistanceBetweenToPoints(endPoint);
+
+            Assert.AreEqual(expected: 0, actual: actual, message: "Maximum value point");
+        }
+
+        [TestMethod]
+        public void TestDistanceToPointZeroValue()
+        {
+            var startPoint = new Point(0d, 0d);
+            var endPoint = new Point(0d, 0d);
+            var actual = startPoint.DistanceBetweenToPoints(endPoint);
+
+            Assert.AreEqual(expected: 0d, actual: actual, message: "Zero value point");
+        }
+
+        [TestMethod]
+        public void DistanceBetweenToPointsNull()
+        {
+            var startPoint = new Point(0d, 0d);
+
+            var actual = startPoint.DistanceBetweenToPoints(null);
+
+            Assert.AreEqual(expected: 0, actual: actual, message: "Null value point");
+        }
+
+        [TestMethod]
+        public void TestDistanceBetweenToMoscowToWashington()
+        {
+            var moscow = new Point(55.752890, 37.617308);
+            var washington = new Point(38.89511, -77.03637);
+            var actual = moscow.DistanceBetweenToPoints(washington);
+
+            Assert.AreEqual(expected: 7822.190, actual: actual, delta: 0.3);
+        }
+
+        [TestMethod]
+        public void TestDistanceBetweenToNovosibirskToDenver()
+        {
+            var novosibirsk = new Point(55.0415, 82.9346);
+            var denver = new Point(39.73915, -104.9847);
+            var actual = novosibirsk.DistanceBetweenToPoints(denver);
+
+            Assert.AreEqual(expected: 9449.09, actual: actual, delta: 0.3);
+        }
+
+        [TestMethod]
+        public void TestStaticDistanceBetweenToMoscowToWashington()
+        {
+            double moscowLatitude = 55.752890;
+            double moscowLongitude = 37.617308;
+            double washingtonLatitude = 38.89511;
+            double washingtonLongitud = -77.03637;
+            var actual = Point.DistanceBetweenToPoint(moscowLatitude, moscowLongitude, washingtonLatitude, washingtonLongitud);
+
+            Assert.AreEqual(expected: 7822.190, actual: actual, delta: 0.3);
+        }
+
+        [TestMethod]
+        public void TestStaticDistanceBetweenToNovosibirskToDenver()
+        {
+            double nskLatitude = 55.0415;
+            double nskLongitude = 82.9346;
+            double denverLatitude = 39.73915;
+            double denverLongitud = -104.9847;
+            var actual = Point.DistanceBetweenToPoint(nskLatitude, nskLongitude, denverLatitude, denverLongitud);
+
+            Assert.AreEqual(expected: 9449.09, actual: actual, delta: 0.3);
+        }
+
+    }
+}
